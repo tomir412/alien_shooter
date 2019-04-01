7badc2f18140222359225dd160e6b453ca81216d
tomir412 <tomasz.rybczynski@onet.pl>
2019-04-01 18:46:00 +0200

    Added settings class and spaceship image

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():
     # initialize game and create screen object
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Set background color to light-gray
+    bg_color = (230, 230, 230)
 
     # Main loop for the game
     while True:
@@ -15,6 +21,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # Redraw the screen each pass trough loop
+        screen.fill(ai_settings.bg_color)
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    """All setting in the game"""
+    def __init__(self):
+        """Initialize's the game's settings"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
