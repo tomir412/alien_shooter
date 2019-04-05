a9da80abb17cf72c2f70ad1410f157f595166dcb
tomir412 <tomasz.rybczynski@onet.pl>
2019-04-05 18:37:28 +0200

    Added firing bullets

--- game_functions.py
+++ game_functions.py
@@ -1,9 +1,30 @@
 import sys
-
 import pygame
 
+from bullet import Bullet
 
-def check_events(ship):
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    """Respond to keypresses"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Create new bullet and add it to the bullets group
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+
+def check_keyup_events(event, ship):
+    """Respond to key releases"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypresses and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -11,24 +32,24 @@ def check_events(ship):
 
         # Smooth movement to left and right
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, alien):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """Update images on the screen"""
     # Redraw the screen each pass trough loop
     screen.fill(ai_settings.bg_color)
+
+    # Redraw all bullets behind ship and aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     ship.blitme()
     alien.blitme()
 
     # Make the most recently drawn screen visible
     pygame.display.flip()
+
